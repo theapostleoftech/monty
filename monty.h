2bf3198396925bb70a8ae65f46d22dfe3c87e8ac
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-01-17 19:25:40 +0100

    Monty Header File

--- monty.h
+++ monty.h
@@ -10,7 +10,8 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
-typedef struct stack_s {
+typedef struct stack_s
+{
     int n;
     struct stack_s *prev;
     struct stack_s *next;
@@ -27,6 +28,7 @@ typedef struct stack_s {
 typedef struct instruction_s
 {
     char *opcode;
+
     void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
