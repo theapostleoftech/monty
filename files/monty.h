b0a29fd31ff29f9e1a3c5f6e1c8b513d2d56b5b7
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-01-18 13:09:05 -0800

    Monty files

--- files/monty.h
+++ files/monty.h
@@ -66,7 +66,16 @@ void monty_free(stack_t *stack);
 int _run(char *line, stack_t **stack, unsigned int line_number, FILE *file);
 void monty_add_node(stack_t **stack, int element);
 void monty_add_queue(stack_t **stack, int element);
+void monty_sub(stack_t **stack, unsigned int line_number);
+void monty_div(stack_t **stack, unsigned int line_number);
+void monty_mul(stack_t **stack, unsigned int line_number);
+void monty_mod(stack_t **stack unsigned int line_number);
+void monty_pchar(stack_t **stack unsigned int line_number);
+void monty_pstr(stack_t **stack unsigned int line_number);
+void monty_rotl(stack **stack unsigned int line_number);
+void monty_rotr(stack_t **stack unsigned int line_number);
 void monty_stack(stack_t **stack, unsigned int line_number);
 void monty_queue(stack_t **stack, unsigned int line_number);
+
 
 #endif
